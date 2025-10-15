a48c752577de1aad8e929f114d3f0b1c6ea4f716
Diego <diegocayax202231074@cunoc.edu.gt>
2025-10-15 01:34:15 -0600

    creacion del proyecto

---
+++ BlibiotecaMagica/src/main/java/com/mycompany/blibiotecamagica/backend/RedBibliotecas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.blibiotecamagica.backend;
+
+/**
+ *
+ * @author rafael-cayax
+ */
+public enum RedBibliotecas {
+    INSTANCIA;
+
+    private RedBibliotecas() {
+        
+    }
+    
+}
